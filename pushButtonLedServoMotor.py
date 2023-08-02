8d0936c373e152a74928cad7cbeb580775826764
Pieter Verheye <pieter.verheye@student.howest.be>
2023-08-02 23:06:12 +0200

    troubleshooting multiple rotations

--- pushButtonLedServoMotor.py
+++ pushButtonLedServoMotor.py
@@ -28,47 +28,39 @@ kit = ServoKit(channels=16)
 servo_channels = [0, 1, 2, 3]  # Adjust this based on your setup
 
 
-# Set the speed of the continuous rotation servo
-def setServoAngle(channel, angle):
-    kit.servo[channel].angle = angle
+# Function to set servo angles and directions
+def setServoAngle(channel, angle, direction):
+    if direction == "forward":
+        kit.servo[channel].angle = angle
+    elif direction == "backward":
+        kit.servo[channel].angle = 180 - angle  # Adjusted angle for backward movement
+    time.sleep(0.1)
+
+
+# Function to rotate the servo at a specific speed
+def setServoSpeed(channel, speed, direction):
+    if direction == "forward":
+        kit.servo[channel].angle += speed
+    elif direction == "backward":
+        kit.servo[channel].angle -= speed
     time.sleep(0.1)
 
 
 # Function to stop the servo motor
 def stopServo(channel):
     kit.servo[channel].angle = None
-try:
 
-    # Start the first motor
-    setServoAngle(servo_channels[0], 90)
-    time.sleep(5)  # Run for 5 seconds
-    stopServo(servo_channels[0])
+    try:
+        # Start the first motor forward
+        setServoAngle(servo_channels[0], 90, "forward")
+        time.sleep(5)  # Run for 5 seconds
 
-    # Wait 5 seconds
-    time.sleep(5)
+        # Rotate the first motor backward
+        setServoSpeed(servo_channels[0], 1, "backward")
+        time.sleep(2)  # Run for 2 seconds
+        stopServo(servo_channels[0])
 
-    # Start the second motor
-    setServoAngle(servo_channels[1], 90)
-    time.sleep(5)  # Run for 5 seconds
-    stopServo(servo_channels[1])
-
-    # Wait 5 seconds
-    time.sleep(5)
-
-    # Start the third motor
-    setServoAngle(servo_channels[2], 90)
-    time.sleep(5)  # Run for 5 seconds
-    stopServo(servo_channels[2])
-
-    # Wait 5 seconds
-    time.sleep(5)
-
-    # Start the fourth motor
-    setServoAngle(servo_channels[3], 90)
-    time.sleep(5)  # Run for 5 seconds
-    stopServo(servo_channels[3])
-
-finally:
-    # Stop all motors before exiting
-    for channel in servo_channels:
-        stopServo(channel)
+    finally:
+        # Stop all motors before exiting
+        for channel in servo_channels:
+            stopServo(channel)
