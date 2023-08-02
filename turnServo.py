51ced96fe13d37c8e57b21274b9a7796f0027995
Pieter Verheye <pieter.verheye@student.howest.be>
2023-08-02 18:04:39 +0200

    updated the program

--- turnServo.py
+++ turnServo.py
@@ -1,31 +1,37 @@
-# Libraries
-
-from adafruit_servokit import ServoKit
 import time
+from adafruit_servokit import ServoKit
 
 # Initialize the servo driver HAT
 kit = ServoKit(channels=16)
 
 # Specify the channels for your servo motors
-servo_channels = [0, 1, 2, 3]
+servo_channels = [0, 1, 2, 3]  # Adjust this based on your setup
 
 
-# Set the servo motors to their initial position
+# Function to set servo angles
 def set_servo_angle(channel, angle):
     kit.servo[channel].angle = angle
     time.sleep(0.1)
 
 
-# Reset servo positions to neutral before exiting
+# Function to stop the servo motor
+def stop_servo(channel):
+    kit.servo[channel].angle = None
+
+
 try:
     # Set servo motors to neutral position (90 degrees)
     for channel in servo_channels:
         set_servo_angle(channel, 90)
 
-    # Keep the servos in the neutral position for a few seconds
-    time.sleep(5)
+    # Keep the servos in the neutral position for 15 seconds
+    time.sleep(15)
+
+    # Stop the servo motors
+    for channel in servo_channels:
+        stop_servo(channel)
 
 finally:
-    # Reset servo positions to 90 degrees before exiting
+    # Reset servo positions before exiting
     for channel in servo_channels:
-        set_servo_angle(channel, 90)
+        stop_servo(channel)
