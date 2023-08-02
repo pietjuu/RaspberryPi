da5397d3685c55321b904ab3f54b1976b110ed1d
Pieter Verheye <pieter.verheye@student.howest.be>
2023-08-02 22:25:10 +0200

    playing with speed

--- pushButtonLedServoMotor.py
+++ pushButtonLedServoMotor.py
@@ -7,7 +7,7 @@ then after 15 seconds the third motor starts, then after 20 seconds the fourth m
 # Libraries
 import RPi.GPIO as GPIO
 from adafruit_servokit import ServoKit
-from time import sleep, time
+import time
 
 # Set warnings off
 GPIO.setwarnings(False)
@@ -25,6 +25,33 @@ GPIO.setup(Led, GPIO.OUT)
 kit = ServoKit(channels=16)
 
 # Specify the channels for your servo motors
-servo_channels = [0, 1, 2, 3]
+servo_channels = [0, 1, 2, 3]  # Adjust this based on your setup
 
 
+# Set the speed of the continuous rotation servo
+def set_servo_speed(channel, speed):
+    kit.continuous_servo[channel].throttle = speed
+
+
+# Stop the continuous rotation servo
+def stop_servo(channel):
+    kit.continuous_servo[channel].throttle = 0
+
+
+def rotateServo():
+    try:
+        for servo_channel in servo_channels:
+            # Rotate the servo at full speed clockwise
+            set_servo_speed(servo_channel, 1)
+            time.sleep(2)  # Run for 2 seconds
+
+        time.sleep(1)  # Pause for 1 second
+
+        for servo_channel in servo_channels:
+            # Rotate the servo at full speed counterclockwise
+            set_servo_speed(servo_channel, -1)
+            time.sleep(2)  # Run for 2 seconds
+
+    finally:
+        # Stop the servo before exiting
+        stop_servo(servo_channels)
--- turnServo.py
+++ turnServo.py
@@ -9,29 +9,33 @@ servo_channels = [0, 1, 2, 3]  # Adjust this based on your setup
 
 
 # Function to set servo angles
-def set_servo_angle(channel, angle):
+def setServoAngle(channel, angle):
     kit.servo[channel].angle = angle
     time.sleep(0.1)
 
 
 # Function to stop the servo motor
-def stop_servo(channel):
+def stopServo(channel):
     kit.servo[channel].angle = None
 
 
-try:
-    # Set servo motors to neutral position (90 degrees)
-    for channel in servo_channels:
-        set_servo_angle(channel, 90)
+def rotateServo():
+    try:
+        # Set servo motors to neutral position (90 degrees)
+        for channel in servo_channels:
+            setServoAngle(channel, 90)
 
-    # Keep the servos in the neutral position for 15 seconds
-    time.sleep(15)
+        # Keep the servos in the neutral position for 15 seconds
+        time.sleep(15)
 
-    # Stop the servo motors
-    for channel in servo_channels:
-        stop_servo(channel)
+        # Stop the servo motors
+        for channel in servo_channels:
+            stopServo(channel)
 
-finally:
-    # Reset servo positions before exiting
-    for channel in servo_channels:
-        stop_servo(channel)
+    finally:
+        # Reset servo positions before exiting
+        for channel in servo_channels:
+            stopServo(channel)
+
+
+rotateServo()
