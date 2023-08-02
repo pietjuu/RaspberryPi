b697f4c3de6b5372b4f7926eb082d6b110888636
Pieter Verheye <pieter.verheye@student.howest.be>
2023-08-02 22:41:13 +0200

    troubleshooting why servo doesnt stop

--- pushButtonLedServoMotor.py
+++ pushButtonLedServoMotor.py
@@ -24,13 +24,8 @@ GPIO.setup(Led, GPIO.OUT)
 # Initialize the servo driver HAT
 kit = ServoKit(channels=16)
 
-# Specify the channels for your servo motors
+# Specify the channels for your continuous rotation servos
 servo_channels = [0, 1, 2, 3]  # Adjust this based on your setup
-
-
-def setServoAngle(channel, angle):
-    kit.servo[channel].angle = angle
-    time.sleep(0.1)
 
 
 # Set the speed of the continuous rotation servo
@@ -40,34 +35,40 @@ def set_servo_speed(channel, speed):
 
 # Stop the continuous rotation servo
 def stop_servo(channel):
+    kit.continuous_servo[channel].throttle = 0
+
+
+try:
+    # Start the first motor
+    set_servo_speed(servo_channels[0], 1)
+    time.sleep(5)  # Run for 5 seconds
+    stop_servo(servo_channels[0])
+
+    # Wait 5 seconds
+    time.sleep(5)
+
+    # Start the second motor
+    set_servo_speed(servo_channels[1], 1)
+    time.sleep(5)  # Run for 5 seconds
+    stop_servo(servo_channels[1])
+
+    # Wait 5 seconds
+    time.sleep(5)
+
+    # Start the third motor
+    set_servo_speed(servo_channels[2], 1)
+    time.sleep(5)  # Run for 5 seconds
+    stop_servo(servo_channels[2])
+
+    # Wait 5 seconds
+    time.sleep(5)
+
+    # Start the fourth motor
+    set_servo_speed(servo_channels[3], 1)
+    time.sleep(5)  # Run for 5 seconds
+    stop_servo(servo_channels[3])
+
+finally:
+    # Stop all motors before exiting
     for channel in servo_channels:
-        setServoAngle(channel, 90)
-
-    print("stop_servo")
-
-
-def rotateServo():
-    try:
-        for servo_channel in servo_channels:
-            # Rotate the servo at full speed clockwise
-            set_servo_speed(servo_channel, 1)
-            time.sleep(2)  # Run for 2 seconds
-
-        time.sleep(1)  # Pause for 1 second
-
-        for servo_channel in servo_channels:
-            # Rotate the servo at full speed counterclockwise
-            set_servo_speed(servo_channel, -1)
-            time.sleep(2)  # Run for 2 seconds
-
-        # Stop the servo motors
-        for channel in servo_channels:
-            stop_servo(channel)
-
-    finally:
-        # Stop the servo before exiting
-        for channel in servo_channels:
-            stop_servo(channel)
-
-
-rotateServo()
+        stop_servo(channel)
